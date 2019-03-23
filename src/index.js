674320f6f6681ace0d5c1f51df92ed72d4d35318
SEBSOUNdS <sebastian.udden@gmail.com>
2019-03-23 19:53:45 +0100

    Setup mongodb connection and initialized data

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
-import models from './models';
+import models, { connectDb } from './models';
+import { connect } from 'http2';
 
 const app = express();
 app.use(cors());
@@ -22,7 +23,56 @@ app.use('/session', routes.session);
 app.use('/users', routes.users);
 app.use('/messages', routes.messages);
 
-// Listen
-app.listen(process.env.PORT, () =>
-    console.log(`Example app listening on port ${process.env.PORT}...`),
-);
+connectDb().then(async () => {
+    if (process.env.ERASE_DB_ON_SYNC) {
+        await Promise.all([
+            models.User.deleteMany({}),
+            models.Message.deleteMany({}),
+        ]);
+    }
+
+    if (process.env.SEED_DB_WITH_USERS_AND_MESSAGES) {
+        createUsersWithMessages();
+    }
+    app.listen(process.env.PORT, () =>
+        console.log(`Example app listening on port ${process.env.PORT}...`),
+    );
+});
+
+const createUsersWithMessages = async () => {
+    const user1 = new models.User({
+        username: 'Sebbe',
+    });
+    const user2 = new models.User({
+        username: 'Henke',
+    });
+
+    const message1 = new models.Message({
+        text: 'Setup the MEN structure is complete!',
+        user: user1.id,
+    });
+
+    const message2 = new models.Message({
+        text: 'Maybe time to add the R to complete MERN?',
+        user: user2.id,
+    });
+
+    const message3 = new models.Message({
+        text: 'By R, I mean React =)',
+        user: user2.id,
+    });
+
+    const message4 = new models.Message({
+        text:
+            'Nice, that means MongoDb, Express, React and Node. Full Stack baby!',
+        user: user1.id,
+    });
+
+    await message1.save();
+    await message2.save();
+    await message3.save();
+    await message4.save();
+
+    await user1.save();
+    await user2.save();
+};
--- src/models/index.js
+++ src/models/index.js
@@ -1,28 +1,13 @@
-let users = {
-    1: {
-        id: '1',
-        username: 'Robin Wieruch',
-    },
-    2: {
-        id: '2',
-        username: 'Dave Davids',
-    },
+import mongoose from 'mongoose';
+
+import User from './user';
+import Message from './message';
+
+const connectDb = () => {
+    return mongoose.connect(process.env.DATABASE_URL);
 };
 
-let messages = {
-    1: {
-        id: '1',
-        text: 'Hello World',
-        userId: '1',
-    },
-    2: {
-        id: '2',
-        text: 'Bye World',
-        userId: '2',
-    },
-};
+const models = { User, Message };
+export { connectDb };
 
-export default {
-    users,
-    messages,
-};
+export default models;
