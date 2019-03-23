04155f51c7883a687f25af9e68a4b2d9a74f2518
SEBSOUNdS <sebastian.udden@gmail.com>
2019-03-23 18:50:47 +0100

    Full modularized API setup with mock db

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import 'dotenv/config';
+import uuidv4 from 'uuid/v4';
+import bodyParser from 'body-parser';
 import express from 'express';
 import cors from 'cors';
+import routes from './routes';
+import models from './models';
 
 const app = express();
 app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('Hello world!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    req.context = {
+        models,
+        me: models.users[1],
+    };
+    next();
 });
 
+app.use('/session', routes.session);
+app.use('/users', routes.users);
+app.use('/messages', routes.messages);
+
+// Listen
 app.listen(process.env.PORT, () =>
     console.log(`Example app listening on port ${process.env.PORT}...`),
 );
